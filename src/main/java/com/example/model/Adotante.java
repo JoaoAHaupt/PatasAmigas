2ab46f291693012e1f29e9a3e2cc6c9135eeb8a4
JoaoAHaupt <joao.haupt@aluno.ifsp.edu.br>
2024-08-19 11:28:33 -0300

    lepo

---
+++ src/main/java/com/example/model/Adotante.java
@@ -0,0 +1,41 @@
+package com.example.model;
+
+import java.util.Date;
+
+public class Adotante extends Pessoa {
+
+    private int idAdotante;
+    private String preferenciasDeAdocao;
+    private String adocoes;
+
+    public Adotante(String nome, Date dataDeNascimento, String genero, String cpf, String endereco, String telefone, String email, String senha, int idAdotante, String preferenciasDeAdocao, String adocoes) {
+        super(nome, dataDeNascimento, genero, cpf, endereco, telefone, email, senha);
+        this.idAdotante = idAdotante;
+        this.preferenciasDeAdocao = preferenciasDeAdocao;
+        this.adocoes = adocoes;
+    }
+
+    public int getIdAdotante() {
+        return idAdotante;
+    }
+
+    public void setIdAdotante(int idAdotante) {
+        this.idAdotante = idAdotante;
+    }
+
+    public String getPreferenciasDeAdocao() {
+        return preferenciasDeAdocao;
+    }
+
+    public void setPreferenciasDeAdocao(String preferenciasDeAdocao) {
+        this.preferenciasDeAdocao = preferenciasDeAdocao;
+    }
+
+    public String getAdocoes() {
+        return adocoes;
+    }
+
+    public void setAdocoes(String adocoes) {
+        this.adocoes = adocoes;
+    }
+}
